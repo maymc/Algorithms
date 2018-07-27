89b682427c06d13e6871f240a6732bf0f533c77c
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-27 00:21:47 -1000

    Implemented and completed insertion sort algorithm

---
+++ insertionSort.js
@@ -0,0 +1,21 @@
+let testArr = [5, 3, 8, 4, 1, 9];
+
+function insertionSort(arr) {
+  console.log(arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    while (arr[i] > arr[i + 1]) {
+      let temp = arr[i];
+      arr[i] = arr[i + 1];
+      arr[i + 1] = temp;
+
+      if (i !== 0) {
+        i--;
+      }
+      console.log("-->", arr);
+    }
+  }
+  return arr;
+}
+
+console.log(insertionSort(testArr));
