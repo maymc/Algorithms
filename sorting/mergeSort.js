51ce85b9596b386a4c48e970a2094d0eaf25060d
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-31 00:23:16 -1000

    Got mergesort to work correctly. Returns sorted array.

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -44,47 +44,64 @@ let testArr = [5, 3, 8, 4, 1, 9, 6];
 //   console.log(testArr2);
 //   console.log(sorted);
 // }
-let singles = [];
 
 function mergeSort(arr) {
-  let arrayCopy = arr.slice(0);
-  console.log("CHECK - array copy: ", arrayCopy);
+  console.log("CHECK - array: ", arr);
 
-  let middle = Math.floor(arrayCopy.length / 2);
-  console.log(
-    "DEBUG - middle index: " + middle + " // middle element: ",
-    arrayCopy[middle]
-  );
+  let middle = Math.floor(arr.length / 2);
+  console.log("DEBUG - middle index: " + middle + " // middle element: ", arr[middle]);
 
   let left = [];
   let right = [];
 
   //If there is only one element in the array, it is already sorted
-  if (arrayCopy.length <= 1) {
+  if (arr.length <= 1) {
     console.log("----->This is single element. It is sorted<--------");
     // singles.push(arrayCopy);
     // console.log(singles);
-    return arrayCopy;
+    return arr;
   }
   //Else
   else {
     //Split the array in half
 
-    left = arrayCopy.slice(0, middle);
+    left = arr.slice(0, middle);
     console.log("CHECK - LEFT Half: ", left);
 
-    right = arrayCopy.slice(middle);
+    right = arr.slice(middle);
     console.log("CHECK - RIGHT Half: ", right);
   }
 
   console.log("~~~~~~~~~~~~~~~");
-  return mergeSort(left).concat(mergeSort(right));
+  return merge(mergeSort(left), mergeSort(right));
 }
 
 mergeSort(testArr);
 
 function merge(leftArr, rightArr) {
   let sorted = [];
-  if (leftArr[0] > rightArr[0]) {
+
+  while (leftArr.length !== 0 && rightArr.length !== 0) {
+    if (leftArr[0] <= rightArr[0]) {
+      //If the left element is smaller than the right element, shift it out of the array and push it into the sorted array
+      sorted.push(leftArr.shift());
+    }
+    else {
+      //else, if the right element is greater than the left element, shift it out of the array and put it into the sorted array
+      sorted.push(rightArr.shift());
+    }
   }
+
+  while (leftArr.length !== 0) {
+    sorted.push(leftArr.shift());
+  }
+
+  while (rightArr.length !== 0) {
+    sorted.push(rightArr.shift());
+  }
+
+  console.log(sorted);
+  return sorted;
 }
+
+
