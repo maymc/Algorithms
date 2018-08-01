64ed7183ee66b283dfb8d2c7e622effdff47c860
May Chen <maychen@Mays-MacBook-Pro.local>
2018-07-31 21:30:21 -1000

    Removed old code and cleaned up final code a bit

--- sorting/mergeSort.js
+++ sorting/mergeSort.js
@@ -1,49 +1,4 @@
 let testArr = [5, 3, 8, 4, 1, 9, 6];
-// let testArr2 = [];
-// let sorted = [];
-
-// function mergeSort(arr) {
-//   let left = [];
-//   let right = [];
-//   let middle = 0;
-
-//   if (arr.length === 1) {
-//     return arr;
-//   } else {
-//     middle = Math.floor(arr.length / 2);
-
-//     left = arr.slice(0, middle);
-//     console.log("left: ", left);
-//     mergeSort(left);
-//     if (left.length === 1) {
-//       testArr2.push(left);
-//       console.log("testArr2: ", testArr2);
-//     }
-
-//     right = arr.slice(middle);
-//     console.log("right: ", right);
-//     mergeSort(right);
-//     if (right.length === 1) {
-//       testArr2.push(right);
-//       console.log("testArr2 here: ", testArr2);
-//     }
-//   }
-
-//   //take the first 2 elements and compare them
-//   if (testArr2[0] > testArr2[1]) {
-//     let temp = testArr2[0];
-//     testArr2[0] = testArr2[1];
-//     testArr2[1] = temp;
-
-//     let shifted = testArr2.shift();
-//     sorted.push(shifted);
-
-//     shifted = testArr2.shift();
-//     sorted.push(shifted);
-//   }
-//   console.log(testArr2);
-//   console.log(sorted);
-// }
 
 function mergeSort(arr) {
   console.log("CHECK - array: ", arr);
@@ -57,14 +12,10 @@ function mergeSort(arr) {
   //If there is only one element in the array, it is already sorted
   if (arr.length <= 1) {
     console.log("----->This is single element. It is sorted<--------");
-    // singles.push(arrayCopy);
-    // console.log(singles);
     return arr;
   }
-  //Else
   else {
     //Split the array in half
-
     left = arr.slice(0, middle);
     console.log("CHECK - LEFT Half: ", left);
 
